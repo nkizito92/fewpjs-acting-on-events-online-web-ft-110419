4a90576f6d90c881d7e30d3beb74045eb13823c7
Kizito Njoku <nkizito57@gmail.com>
2020-03-03 22:06:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+
+let dodger = document.querySelector("#dodger")
+
+dodger.style.backgroundColor = "green";
+dodger.style.left = "180px";
+dodger.style.bottom = "0px";
+dodger.style.right = "180px";
+
+// document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft") {
+//     moveDodgerLeft();
+//   }
+// });
+
+document.addEventListener("keydown", function(r) {
+  if (r.key === "ArrowRight") {
+     
+     moveDodgerRight();
+   }
+});
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 2}px`;
+  }
+}
+
+function moveDodgerRight () {
+  let rightNum = dodger.style.right.replace("px", "")
+  let right = parseInt(rightNum, 10)
+  if (right >  0) {
+    dodger.style.left = `${right + 2}px`;
+  } 
+}
