b9e1fa0827d54b45c44d5386ddc2e72739d45998
Kizito Njoku <nkizito57@gmail.com>
2020-03-03 22:09:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ function moveDodgerup () {
   let rightNum = dodger.style.top.replace("px", "")
   let right = parseInt(rightNum, 10)
   if (right >  0) {
-    dodger.style.left = `${right + 2}px`;
+    dodger.style.top = `${right + 2}px`;
   } 
 }
